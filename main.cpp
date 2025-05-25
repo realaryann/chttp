50d7ce6894dcd1b5e20cd13835811d6694cb76fc
Aryan Nagpal <arcnagpal1234@gmail.com>
2025-05-24 20:49:20 -0400

    CHTTP v0.0.2: Added set_socket_options to allow reuse of addresses

--- main.cpp
+++ main.cpp
@@ -15,17 +15,36 @@ Accept – When a client connects, accept the connection and get a new socket to
 
 */
 
+const int PORT=8080;
+
+int create_socket();
+void set_socket_options(int skt);
+
+int main() {
+    int skt = create_socket();
+
+    set_socket_options(skt);
+
+    return 0;
+}
+
 int create_socket() {
     // Create a TCP socket
     int res = socket(AF_INET, SOCK_STREAM, 0);
-    if (res == 0) {
-        perror("create_socket() failed");
-        exit(1);
+    if (res == -1) {
+        std::cerr << "create_socket() failed";
+        exit(EXIT_FAILURE);
     }
     return res;
 }
 
-int main() {
-    int skt = create_socket();
-    return 0;
+void set_socket_options(int skt) {
+    int optval = 1;
+
+    if (setsockopt(skt, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        std::cerr << "set_socket_options failed";
+        close(skt);
+        exit(EXIT_FAILURE);
+    }
+
 }
