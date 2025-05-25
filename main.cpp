951ffe84b7572b7cfa41a93c9315cded97f60f86
Aryan Nagpal <arcnagpal1234@gmail.com>
2025-05-25 10:20:26 -0400

    CHTTP v0.0.3: Added methods to bind socket to port, and listen to connections

--- main.cpp
+++ main.cpp
@@ -16,16 +16,48 @@ Accept – When a client connects, accept the connection and get a new socket to
 */
 
 const int PORT=8080;
+const int BACKLOG = 10;
 
 int create_socket();
 void set_socket_options(int skt);
+void bind_socket_to_port(int skt, int port);
+void start_listening(int skt);
 
 int main() {
     int skt = create_socket();
 
     set_socket_options(skt);
 
+    bind_socket_to_port(skt, PORT);
+
+    start_listening(skt);
+
     return 0;
+}
+
+void start_listening(int skt) {
+    if (listen(skt, BACKLOG) < 0) {
+        std::cerr << "start_listening() failed";
+        close(skt);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void bind_socket_to_port(int skt, int port) {
+    // IPv4 Structure
+    sockaddr_in address;
+    std::memset(&address,0,sizeof(address));
+
+    address.sin_family = AF_INET; // Set to IPv4 family
+    address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP
+    address.sin_port = htons(port); // convert port to network byte order
+
+    if (bind(skt, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "bind_socket_to_port() failed";
+        close(skt);
+        exit(EXIT_FAILURE);
+    }
+
 }
 
 int create_socket() {
